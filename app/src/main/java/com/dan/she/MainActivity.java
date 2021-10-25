e9381b64f2f3ca9f22445636ff3d7cb1882b3bf0
Joy Singh <avnishmamta060606@gmail.com>
2021-10-25 14:14:15 +0530

    push notif fixed

--- app/src/main/java/com/dan/she/MainActivity.java
+++ app/src/main/java/com/dan/she/MainActivity.java
@@ -11,8 +11,20 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.installations.FirebaseInstallations;
+import com.google.firebase.installations.InstallationTokenResult;
 import com.google.firebase.messaging.FirebaseMessaging;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import java.util.ArrayList;
 
@@ -29,24 +41,7 @@ public class MainActivity extends AppCompatActivity {
 //        String V1 = extras.getString(Intent.EXTRA_TEXT);
 //        Log.d("NumberMainActivity", V1);
 
-        FirebaseMessaging.getInstance().getToken()
-                .addOnCompleteListener(new OnCompleteListener<String>() {
-                    @Override
-                    public void onComplete(@NonNull Task<String> task) {
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
-                            return;
-                        }
-
-                        // Get new FCM registration token
-                        String token = task.getResult();
-
-                        // Log and toast
-                        String msg = getString(R.string.msg_token_fmt, token);
-                        Log.d(TAG, msg);
-                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                    }
-                });
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
     }
 
     @Override
@@ -92,6 +87,12 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+    public void invokeSoS(){
+        FcmNotificationsSender notificationsSender = new FcmNotificationsSender("/topics/all", "SHE", "Help please...!!", getApplicationContext(), MainActivity.this);
+
+        notificationsSender.SendNotifications();
+    }
+
 
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
@@ -99,16 +100,18 @@ public class MainActivity extends AppCompatActivity {
         int key = event.getKeyCode();
         switch (key) {
             case (KeyEvent.KEYCODE_VOLUME_DOWN):
-                if(event.getRepeatCount() >= 100){
+                if(event.getRepeatCount() == 100){
                     // Do long press event here
-                    Log.d("Down Pressed", event.toString());
+                    invokeSoS();
+//                    Log.d("Down Pressed", event.toString());
                     Toast.makeText(getApplicationContext(),"SOS sent successfully..",Toast.LENGTH_SHORT).show();
                 }
 
             case (KeyEvent.KEYCODE_VOLUME_UP):
-                if(event.getRepeatCount() >= 100){
+                if(event.getRepeatCount() == 100){
                     // Do long press event here
-                    Log.d("Up Pressed", event.toString());
+                    invokeSoS();
+//                    Log.d("Up Pressed", event.toString());
                     Toast.makeText(getApplicationContext(),"SOS sent successfully..",Toast.LENGTH_SHORT).show();
                 }
         }
